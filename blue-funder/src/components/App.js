f7f3ebdc70e40ea7af6e90b1f7502406414c362d
KeithSRoberts <robertsskeith@gmail.com>
2020-02-15 14:26:46 -0800

    Cleaned up project directory structure

---
+++ blue-funder/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component }  from "react";
+import '../css/App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h1>Blue Funder Portal</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
