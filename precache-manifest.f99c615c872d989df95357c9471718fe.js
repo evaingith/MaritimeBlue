d699431ea737a9ee8a3b477d9160ad3717061951
KeithSRoberts <robertsskeith@gmail.com>
2020-02-14 19:34:04 -0800

    Updates

---
+++ precache-manifest.f99c615c872d989df95357c9471718fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e76c73a783c5a6d75bf24e92f56b4c4",
+    "url": "/MaritimeBlue/index.html"
+  },
+  {
+    "revision": "5c9610cb7e31ff07a78f",
+    "url": "/MaritimeBlue/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "18a131ec7572275c9815",
+    "url": "/MaritimeBlue/static/js/2.cadeb2bf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MaritimeBlue/static/js/2.cadeb2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9610cb7e31ff07a78f",
+    "url": "/MaritimeBlue/static/js/main.9b21a4ec.chunk.js"
+  },
+  {
+    "revision": "a3d4881b5ea080432e88",
+    "url": "/MaritimeBlue/static/js/runtime-main.9a1a484c.js"
+  }
+]);
