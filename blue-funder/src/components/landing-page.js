db6ccc2701709ef7cbde7f925412e2782dd425d1
KeithSRoberts <robertsskeith@gmail.com>
2020-02-21 12:09:53 -0800

    Added links for ischool and sponsor, added paragraph about why users should care

--- blue-funder/src/components/landing-page.js
+++ blue-funder/src/components/landing-page.js
@@ -45,8 +45,9 @@ class LandingPage extends Component {
         <a name="features"></a>
         <section className="features">
           <h3 className="title">How It Works</h3>
-          <p>Currently this section is reserved for images and information about various features of our application, but we are still implementing those features! Check back later...</p>
+          <p>Innovators and entrepreneurs who are involved with the Maritime sector discover new ways to make the industry more "Blue" each day, encouraging values of sustainability and efficiency within their projects. Finding resources to promote these projects is a monumental task alone, but is critical to pushing the Maritime community towards more eco-friendly practices and ideas. Through this portal, innovators can be directly connected to a widely comprehensive and robust set of investment oppurtunities that match their goals in order to achieve the funding they need.</p>
           <hr></hr>
+          <p>Currently this section is reserved for images and information about various features of our application, but we are still implementing those features! Check back later...</p>
         </section>
 
 
@@ -88,13 +89,13 @@ class LandingPage extends Component {
               <img alt="sponsor logo" className="sponsor-img" src="https://maritimeblue.org/wp-content/uploads/2019/01/WAMarBlue_CLR_v3.1C.110717.png" />
             </div>
             <div>
-              <p>Department of Commerce - Washington State & The Maritime Blue Project</p>
+              <p><a href="https://maritimeblue.org/">Department of Commerce - Washington State & The Maritime Blue Project</a></p>
             </div>
           </div>
         </section>
 
         <footer>
-          <p>This project is a part of the Capstone Project course at the University of Washington Information School</p>
+          <p><a href="https://ischool.uw.edu/">This project is a part of the Capstone Project course at the University of Washington Information School</a></p>
         </footer>
 
       </div>
